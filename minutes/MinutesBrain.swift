863ef4e961b7619fa888586f07bd4d7d20934fab
Sunny Nie <sunnymingnie@gmail.com>
2020-12-18 17:19:20 -0800

    test commit

---
+++ minutes/MinutesBrain.swift
@@ -0,0 +1,9 @@
+//
+//  MinutesBrain.swift
+//  minutes
+//
+//  Created by Sunny Nie on 2020-12-17.
+//  Copyright © 2020 Sunny Nie. All rights reserved.
+//
+
+import Foundation
---
+++ minutes/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  minutes
+//
+//  Created by Sunny Nie on 2020-12-17.
+//  Copyright © 2020 Sunny Nie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
